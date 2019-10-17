ffcb0668faab62ee7ae21a1779c100fd526642da
Victor Mourão <victor_murati@hotmail.com>
2019-10-16 21:07:39 -0300

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  /*
+    Com o array vazio, substitui o componentDidMount, pois como não tem nenhum estado
+    sendo observado, ele roda somente uma vez.
+  */
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+    /* retornar uma função é a forma de fazer este useEffect ser executado quando
+    o componente deixa de ser montado(componentWillUnmount)
+    */
+    // return () => {};
+  }, []);
+
+  /*
+    Estado tech esta sendo observado e quando tiver alguma alteração, useEffect
+    executa o escopo da função.(componentDidUpdate)
+  */
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
